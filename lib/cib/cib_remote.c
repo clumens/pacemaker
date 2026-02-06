c16141478dfb8c0300a048b3bf578b799eafc923
Chris Lumens <clumens@redhat.com>
2026-02-06 10:01:52 -0500

    API: libcrmcommon: Deprecate remote-clear-port.
    This CIB property is deprecated in favor of remote-tls-port which is
    more secure and has been supported since at least 2014.  For now, it
    will still be recognized but will log a warning.  Also mark it as
    deprecated in the docs and remove references to it being something that
    you can use.
    
    Ref T961

--- lib/cib/cib_remote.c
+++ lib/cib/cib_remote.c
@@ -421,6 +421,10 @@ cib_tls_signon(cib_t *cib, pcmk__remote_t *connection, gboolean event_channel)
             cib_tls_close(cib);
             return -1;
         }
+    } else {
+        pcmk__warn("Connecting to remote CIB without encryption. This is "
+                   "insecure and will be removed in a future release. Use "
+                   "the " PCMK_XA_REMOTE_TLS_PORT " cluster option instead.");
     }
 
     /* Now that the handshake is done, see if any client TLS certificate is
