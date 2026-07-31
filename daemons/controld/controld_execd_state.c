clumens/pacemaker

commit e23d1b6a775db219f090ce136d76100efb1dbfe7

API: liblrmd: lrmd_event_data_t char * members are now non-const
lrmd_new_event() allocates memory for rsc_id and op_type.

lrmd_copy_event() allocates memory for rsc_id, op_type, user_data,
output, remote_nodename, and exit_reason.

lrmd_dispatch_internal() allocates memory for output and exit_reason,
but prior to this commit it used strings belonging to other objects for
rsc_id, op_type, user_data, and remote_nodename.

lrmd_free_event() frees all six fields.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/controld/controld_execd_state.c
@@ -55,31 +55,28 @@ free_recurring_op(void *value)
 static gboolean
 fail_pending_op(void *key, void *value, void *user_data)
 {
-    lrmd_event_data_t event = { 0, };
     lrm_state_t *lrm_state = user_data;
     active_op_t *op = value;
+    lrmd_event_data_t *event = lrmd_new_event(op->rsc_id, op->op_type,
+                                              op->interval_ms);
 
     pcmk__trace("Pre-emptively failing " PCMK__OP_FMT " on %s (call=%s, %s)",
                 op->rsc_id, op->op_type, op->interval_ms,
                 lrm_state->node_name, (const char *) key, op->user_data);
 
-    event.type = lrmd_event_exec_complete;
-    event.rsc_id = op->rsc_id;
-    event.op_type = op->op_type;
-    event.user_data = op->user_data;
-    event.timeout = 0;
-    event.interval_ms = op->interval_ms;
-    lrmd__set_result(&event, PCMK_OCF_UNKNOWN_ERROR, PCMK_EXEC_NOT_CONNECTED,
+    event->type = lrmd_event_exec_complete;
+    event->user_data = pcmk__str_copy(op->user_data);
+    lrmd__set_result(event, PCMK_OCF_UNKNOWN_ERROR, PCMK_EXEC_NOT_CONNECTED,
                      "Action was pending when executor connection was dropped");
-    event.t_run = op->start_time;
-    event.t_rcchange = op->start_time;
+    event->t_run = op->start_time;
+    event->t_rcchange = op->start_time;
 
-    event.call_id = op->call_id;
-    event.remote_nodename = lrm_state->node_name;
-    event.params = op->params;
+    event->call_id = op->call_id;
+    event->remote_nodename = pcmk__str_copy(lrm_state->node_name);
+    event->params = op->params;
 
-    process_lrm_event(lrm_state, &event, op, NULL);
-    lrmd__reset_result(&event);
+    process_lrm_event(lrm_state, event, op, NULL);
+    lrmd_free_event(event);
     return TRUE;
 }
 

daemons/controld/controld_execd.c
@@ -1729,11 +1729,13 @@ controld_ack_event_directly(const char *to_host, const char *to_sys,
     pcmk__node_status_t *peer = NULL;
 
     CRM_CHECK(op != NULL, return);
+
     if (op->rsc_id == NULL) {
-        // op->rsc_id is a (const char *) but lrmd_free_event() frees it
-        pcmk__assert(rsc_id != NULL);
         op->rsc_id = pcmk__str_copy(rsc_id);
     }
+
+    pcmk__assert(op->rsc_id != NULL);
+
     if (to_sys == NULL) {
         to_sys = CRM_SYSTEM_TENGINE;
     }

daemons/controld/controld_remote_ra.c
@@ -399,26 +399,24 @@ check_remote_node_state(const remote_ra_cmd_t *cmd)
 static void
 report_remote_ra_result(remote_ra_cmd_t * cmd)
 {
-    lrmd_event_data_t op = { 0, };
+    lrmd_event_data_t *event = lrmd_new_event(cmd->rsc_id, cmd->action,
+                                              cmd->interval_ms);
 
     check_remote_node_state(cmd);
 
-    op.type = lrmd_event_exec_complete;
-    op.rsc_id = cmd->rsc_id;
-    op.op_type = cmd->action;
-    op.user_data = cmd->userdata;
-    op.timeout = cmd->timeout;
-    op.interval_ms = cmd->interval_ms;
-    op.t_run = cmd->start_time;
-    op.t_rcchange = cmd->start_time;
+    event->type = lrmd_event_exec_complete;
+    event->user_data = pcmk__str_copy(cmd->userdata);
+    event->timeout = cmd->timeout;
+    event->t_run = cmd->start_time;
+    event->t_rcchange = cmd->start_time;
 
-    lrmd__set_result(&op, cmd->result.exit_status, cmd->result.execution_status,
-                     cmd->result.exit_reason);
+    lrmd__set_result(event, cmd->result.exit_status,
+                     cmd->result.execution_status, cmd->result.exit_reason);
 
     if (pcmk__is_set(cmd->status, cmd_reported_success)
         && !pcmk__result_ok(&cmd->result)) {
 
-        op.t_rcchange = time(NULL);
+        event->t_rcchange = time(NULL);
         /* This edge case will likely never ever occur, but if it does the
          * result is that a failure will not be processed correctly. This is only
          * remotely possible because we are able to detect a connection resource's tcp
@@ -428,27 +426,25 @@ report_remote_ra_result(remote_ra_cmd_t * cmd)
          * basically, we are not guaranteed that the first successful monitor op and
          * a subsequent failed monitor op will not occur in the same timestamp. We have to
          * make it look like the operations occurred at separate times though. */
-        if (op.t_rcchange == op.t_run) {
-            op.t_rcchange++;
+        if (event->t_rcchange == event->t_run) {
+            event->t_rcchange++;
         }
     }
 
     if (cmd->params) {
         lrmd_key_value_t *tmp;
 
-        op.params = pcmk__strkey_table(free, free);
+        event->params = pcmk__strkey_table(free, free);
         for (tmp = cmd->params; tmp; tmp = tmp->next) {
-            pcmk__insert_dup(op.params, tmp->key, tmp->value);
+            pcmk__insert_dup(event->params, tmp->key, tmp->value);
         }
 
     }
-    op.call_id = cmd->call_id;
-    op.remote_nodename = cmd->owner;
+    event->call_id = cmd->call_id;
+    event->remote_nodename = pcmk__str_copy(cmd->owner);
 
-    lrm_op_callback(&op);
-
-    g_clear_pointer(&op.params, g_hash_table_destroy);
-    lrmd__reset_result(&op);
+    lrm_op_callback(event);
+    lrmd_free_event(event);
 }
 
 /*!
@@ -562,7 +558,7 @@ monitor_timeout_cb(void *data)
 static void
 synthesize_lrmd_success(lrm_state_t *lrm_state, const char *rsc_id, const char *op_type)
 {
-    lrmd_event_data_t op = { 0, };
+    lrmd_event_data_t *event = NULL;
 
     if (lrm_state == NULL) {
         /* if lrm_state not given assume local */
@@ -570,14 +566,13 @@ synthesize_lrmd_success(lrm_state_t *lrm_state, const char *rsc_id, const char *
     }
     pcmk__assert(lrm_state != NULL);
 
-    op.type = lrmd_event_exec_complete;
-    op.rsc_id = rsc_id;
-    op.op_type = op_type;
-    op.t_run = time(NULL);
-    op.t_rcchange = op.t_run;
-    op.call_id = generate_callid();
-    lrmd__set_result(&op, PCMK_OCF_OK, PCMK_EXEC_DONE, NULL);
-    process_lrm_event(lrm_state, &op, NULL, NULL);
+    event = lrmd_new_event(rsc_id, op_type, 0);
+    event->type = lrmd_event_exec_complete;
+    event->t_run = time(NULL);
+    event->t_rcchange = event->t_run;
+    event->call_id = generate_callid();
+    lrmd__set_result(event, PCMK_OCF_OK, PCMK_EXEC_DONE, NULL);
+    process_lrm_event(lrm_state, event, NULL, NULL);
 }
 
 void

include/crm/lrmd_events.h
@@ -43,11 +43,11 @@ typedef struct lrmd_event_data_s {
     enum lrmd_callback_event type;
 
     /*! The resource this event occurred on. */
-    const char *rsc_id;
+    char *rsc_id;
     /*! The action performed, start, stop, monitor... */
-    const char *op_type;
+    char *op_type;
     /*! The user data passed by caller of exec() API function */
-    const char *user_data;
+    char *user_data;
 
     /*! The client api call id associated with this event */
     int call_id;
@@ -71,7 +71,7 @@ typedef struct lrmd_event_data_s {
     int op_status;
 
     /*! stdout from resource agent operation */
-    const char *output;
+    char *output;
 
     /*! Timestamp of when op ran */
     time_t t_run;
@@ -95,10 +95,10 @@ typedef struct lrmd_event_data_s {
     /*! client node name associated with this connection
      * (used to match actions to the proper client when there are multiple)
      */
-    const char *remote_nodename;
+    char *remote_nodename;
 
     /*! exit failure reason string from resource agent operation */
-    const char *exit_reason;
+    char *exit_reason;
 } lrmd_event_data_t;
 
 lrmd_event_data_t *lrmd_new_event(const char *rsc_id, const char *task,

lib/lrmd/lrmd_client.c
@@ -179,7 +179,6 @@ lrmd_new_event(const char *rsc_id, const char *task, unsigned int interval_ms)
 {
     lrmd_event_data_t *event = pcmk__assert_alloc(1, sizeof(lrmd_event_data_t));
 
-    // lrmd_event_data_t has (const char *) members that lrmd_free_event() frees
     event->rsc_id = pcmk__str_copy(rsc_id);
     event->op_type = pcmk__str_copy(task);
     event->interval_ms = interval_ms;
@@ -195,7 +194,6 @@ lrmd_copy_event(const lrmd_event_data_t *event)
 
     copy->type = event->type;
 
-    // lrmd_event_data_t has (const char *) members that lrmd_free_event() frees
     copy->rsc_id = pcmk__str_copy(event->rsc_id);
     copy->op_type = pcmk__str_copy(event->op_type);
     copy->user_data = pcmk__str_copy(event->user_data);
@@ -231,11 +229,11 @@ lrmd_free_event(lrmd_event_data_t *event)
     if (event == NULL) {
         return;
     }
-    // @TODO Why are these const char *?
-    free((void *) event->rsc_id);
-    free((void *) event->op_type);
-    free((void *) event->user_data);
-    free((void *) event->remote_nodename);
+
+    free(event->rsc_id);
+    free(event->op_type);
+    free(event->user_data);
+    free(event->remote_nodename);
     lrmd__reset_result(event);
     g_clear_pointer(&event->params, g_hash_table_destroy);
     free(event);
@@ -284,7 +282,7 @@ lrmd_dispatch_internal(void *data, void *user_data)
     const char *rsc_action = pcmk__xe_get(msg, PCMK__XA_LRMD_RSC_ACTION);
     const char *proxy_session = pcmk__xe_get(msg, PCMK__XA_LRMD_IPC_SESSION);
     lrmd_private_t *native = lrmd->lrmd_private;
-    lrmd_event_data_t event = { 0, };
+    lrmd_event_data_t *event = NULL;
 
     if (proxy_session != NULL) {
         if (native->proxy_callback == NULL) {
@@ -301,68 +299,68 @@ lrmd_dispatch_internal(void *data, void *user_data)
         return;
     }
 
-    event.remote_nodename = native->remote_nodename;
-    pcmk__xe_get_int(msg, PCMK__XA_LRMD_CALLID, &event.call_id);
-    event.rsc_id = rsc_id;
+    event = lrmd_new_event(rsc_id, rsc_action, 0);
+    event->remote_nodename = pcmk__str_copy(native->remote_nodename);
+    pcmk__xe_get_int(msg, PCMK__XA_LRMD_CALLID, &event->call_id);
 
     if (pcmk__str_eq(op, LRMD_OP_RSC_REG, pcmk__str_none)) {
-        event.type = lrmd_event_register;
+        event->type = lrmd_event_register;
 
     } else if (pcmk__str_eq(op, LRMD_OP_RSC_UNREG, pcmk__str_none)) {
-        event.type = lrmd_event_unregister;
+        event->type = lrmd_event_unregister;
 
     } else if (pcmk__str_eq(op, LRMD_OP_RSC_EXEC, pcmk__str_none)) {
         int rc = 0;
         int exec_time = 0;
         int queue_time = 0;
 
-        pcmk__xe_get_int(msg, PCMK__XA_LRMD_TIMEOUT, &event.timeout);
-        pcmk__xe_get_uint(msg, PCMK__XA_LRMD_RSC_INTERVAL, &event.interval_ms);
+        event->type = lrmd_event_exec_complete;
+        pcmk__xe_get_int(msg, PCMK__XA_LRMD_TIMEOUT, &event->timeout);
+        pcmk__xe_get_uint(msg, PCMK__XA_LRMD_RSC_INTERVAL, &event->interval_ms);
         pcmk__xe_get_int(msg, PCMK__XA_LRMD_RSC_START_DELAY,
-                         &event.start_delay);
+                         &event->start_delay);
 
         pcmk__xe_get_int(msg, PCMK__XA_LRMD_EXEC_RC, &rc);
-        event.rc = (enum ocf_exitcode) rc;
+        event->rc = (enum ocf_exitcode) rc;
 
-        pcmk__xe_get_int(msg, PCMK__XA_LRMD_EXEC_OP_STATUS, &event.op_status);
-        pcmk__xe_get_int(msg, PCMK__XA_LRMD_RSC_DELETED, &event.rsc_deleted);
+        pcmk__xe_get_int(msg, PCMK__XA_LRMD_EXEC_OP_STATUS, &event->op_status);
+        pcmk__xe_get_int(msg, PCMK__XA_LRMD_RSC_DELETED, &event->rsc_deleted);
 
-        pcmk__xe_get_time(msg, PCMK__XA_LRMD_RUN_TIME, &event.t_run);
-        pcmk__xe_get_time(msg, PCMK__XA_LRMD_RCCHANGE_TIME, &event.t_rcchange);
+        pcmk__xe_get_time(msg, PCMK__XA_LRMD_RUN_TIME, &event->t_run);
+        pcmk__xe_get_time(msg, PCMK__XA_LRMD_RCCHANGE_TIME, &event->t_rcchange);
 
         pcmk__xe_get_int(msg, PCMK__XA_LRMD_EXEC_TIME, &exec_time);
         CRM_LOG_ASSERT(exec_time >= 0);
-        event.exec_time = QB_MAX(0, exec_time);
+        event->exec_time = QB_MAX(0, exec_time);
 
         pcmk__xe_get_int(msg, PCMK__XA_LRMD_QUEUE_TIME, &queue_time);
-        event.queue_time = QB_MAX(0, queue_time);
+        event->queue_time = QB_MAX(0, queue_time);
 
-        event.op_type = rsc_action;
-        event.user_data = pcmk__xe_get(msg, PCMK__XA_LRMD_RSC_USERDATA_STR);
-        event.type = lrmd_event_exec_complete;
+        event->user_data = pcmk__xe_get_copy(msg,
+                                             PCMK__XA_LRMD_RSC_USERDATA_STR);
 
-        /* output and exit_reason may be freed by a callback */
-        event.output = pcmk__xe_get_copy(msg, PCMK__XA_LRMD_RSC_OUTPUT);
-        lrmd__set_result(&event, event.rc, event.op_status,
+        event->output = pcmk__xe_get_copy(msg, PCMK__XA_LRMD_RSC_OUTPUT);
+        lrmd__set_result(event, event->rc, event->op_status,
                          pcmk__xe_get(msg, PCMK__XA_LRMD_RSC_EXIT_REASON));
 
-        event.params = xml2list(msg);
+        event->params = xml2list(msg);
 
     } else if (pcmk__str_eq(op, LRMD_OP_NEW_CLIENT, pcmk__str_none)) {
-        event.type = lrmd_event_new_client;
+        event->type = lrmd_event_new_client;
 
     } else if (pcmk__str_eq(op, LRMD_OP_POKE, pcmk__str_none)) {
-        event.type = lrmd_event_poke;
+        event->type = lrmd_event_poke;
 
     } else {
-        return;
+        goto done;
     }
 
     pcmk__trace("op %s notify event received", op);
-    native->callback(&event);
 
-    g_clear_pointer(&event.params, g_hash_table_destroy);
-    lrmd__reset_result(&event);
+    native->callback(event);
+
+done:
+    lrmd_free_event(event);
 }
 
 // \return Always 0, to indicate that IPC mainloop source should be kept
@@ -487,16 +485,19 @@ static void
 report_async_connection_result(lrmd_t *lrmd, int rc)
 {
     lrmd_private_t *native = lrmd->lrmd_private;
-    lrmd_event_data_t event = { 0, };
+    lrmd_event_data_t *event = NULL;
 
     if (native->callback == NULL) {
         return;
     }
 
-    event.type = lrmd_event_connect;
-    event.remote_nodename = native->remote_nodename;
-    event.connection_rc = rc;
-    native->callback(&event);
+    event = lrmd_new_event(NULL, NULL, 0);
+    event->type = lrmd_event_connect;
+    event->connection_rc = rc;
+    event->remote_nodename = pcmk__str_copy(native->remote_nodename);
+
+    native->callback(event);
+    lrmd_free_event(event);
 }
 
 static void
@@ -647,7 +648,7 @@ lrmd_ipc_connection_destroy(void *userdata)
 {
     lrmd_t *lrmd = userdata;
     lrmd_private_t *native = lrmd->lrmd_private;
-    lrmd_event_data_t event = { 0, };
+    lrmd_event_data_t *event = NULL;
 
     switch (native->type) {
         case pcmk__client_ipc:
@@ -670,9 +671,12 @@ lrmd_ipc_connection_destroy(void *userdata)
         return;
     }
 
-    event.type = lrmd_event_disconnect;
-    event.remote_nodename = native->remote_nodename;
-    native->callback(&event);
+    event = lrmd_new_event(NULL, NULL, 0);
+    event->type = lrmd_event_disconnect;
+    event->remote_nodename = pcmk__str_copy(native->remote_nodename);
+
+    native->callback(event);
+    lrmd_free_event(event);
 }
 
 /*!
@@ -1098,7 +1102,7 @@ lrmd_tls_connection_destroy(void *userdata)
 {
     lrmd_t *lrmd = userdata;
     lrmd_private_t *native = lrmd->lrmd_private;
-    lrmd_event_data_t event = { 0, };
+    lrmd_event_data_t *event = NULL;
 
     pcmk__info("TLS connection destroyed");
 
@@ -1129,9 +1133,12 @@ lrmd_tls_connection_destroy(void *userdata)
         return;
     }
 
-    event.type = lrmd_event_connect;
-    event.remote_nodename = native->remote_nodename;
-    native->callback(&event);
+    event = lrmd_new_event(NULL, NULL, 0);
+    event->type = lrmd_event_disconnect;
+    event->remote_nodename = pcmk__str_copy(native->remote_nodename);
+
+    native->callback(event);
+    lrmd_free_event(event);
 }
 
 static void
@@ -2437,9 +2444,7 @@ lrmd__set_result(lrmd_event_data_t *event, enum ocf_exitcode rc, int op_status,
 
     event->rc = rc;
     event->op_status = op_status;
-
-    // lrmd_event_data_t has (const char *) members that lrmd_free_event() frees
-    pcmk__str_update((char **) &event->exit_reason, exit_reason);
+    pcmk__str_update(&event->exit_reason, exit_reason);
 }
 
 /*!

commit 634ce06e56bc3b02deeb599911fba956c2c5c9ab

Fix: controller: Avoid double-free from fail_pending_op()
This fixes a regression introduced by e23d1b6. It has not made it into
any release.

As of that commit, event in fail_pending_op() takes shared ownership of
op->params. When lrmd_free_event() frees event, it destroys the
event->params table (which is also the op->params table).

However, fail_pending_op() returns TRUE, which tells
g_hash_table_foreach_remove() to free op after fail_pending_op()
returns. It does this by calling free_recurring_op(), which destroys the
op->params table again.

This double free causes a GLib memory error, and the program terminates.
Below is a partial backtrace to illustrate the issue.

g_malloc0 (n_bytes=11828847412100) at ../glib/gmem.c:138
g_hash_table_resize (hash_table=0x561590a468d0 = {...}) at ../glib/ghash.c:832
g_hash_table_destroy (hash_table=0x561590a468d0 = {...}) at ../glib/ghash.c:1463
free_recurring_op (value=0x561590b61a10) at daemons/controld/controld_execd_state.c:51
g_hash_table_foreach_remove_or_steal (hash_table=0x561590b47a10 = {...}, func=func@entry=0x56157e300000 <fail_pending_op>,
    user_data=user_data@entry=0x561590a38740, notify=notify@entry=1) at ../glib/ghash.c:2011
g_hash_table_foreach_remove (hash_table=<optimized out>, func=func@entry=0x56157e300000 <fail_pending_op>,
    user_data=user_data@entry=0x561590a38740) at ../glib/ghash.c:2055
lrm_state_disconnect_only (lrm_state=0x561590a38740)
    at daemons/controld/controld_execd_state.c:249
lrm_state_disconnect (lrm_state=0x561590a38740)
    at daemons/controld/controld_execd_state.c:259

We observed this issue in the RemoteStonithd cts-lab test.

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/controld/controld_execd_state.c
@@ -73,7 +73,7 @@ fail_pending_op(void *key, void *value, void *user_data)
 
     event->call_id = op->call_id;
     event->remote_nodename = pcmk__str_copy(lrm_state->node_name);
-    event->params = op->params;
+    event->params = pcmk__str_table_dup(op->params);
 
     process_lrm_event(lrm_state, event, op, NULL);
     lrmd_free_event(event);